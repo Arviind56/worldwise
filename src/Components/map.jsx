ebc91fd5fb22a4e339ca31dacb50c5e9c4c8232a
Arvind Rajagopalan <arvindrajagopalan87@gmail.com>
2024-06-14 06:40:21 +0530

    update

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -1,12 +1,32 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
 import styles from "./map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [MapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams([]);
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
 
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition({ mapLat, mapLng });
+    },
+    [mapLat, mapLng]
+  );
+  const flagemojiToPNG = (flag) => {
+    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+      .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+      .join("");
+    return (
+      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+    );
+  };
   return (
     <div
       className={styles.mapContainer}
@@ -14,12 +34,36 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>MAP</h1>
-      <h1>
-        position:{lat},{lng}
-      </h1>
+      <MapContainer
+        center={MapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{flagemojiToPNG(city.emoji)}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={[Map]} />
+      </MapContainer>
     </div>
   );
+  function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+  }
 }
 
 export default Map;
